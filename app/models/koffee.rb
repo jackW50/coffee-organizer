d83f486266c6961184974671e52454e1aca669e1
github username <github email address>
2019-07-17 12:37:19 -0500

    add class search method to Koffee model

--- app/models/koffee.rb
+++ app/models/koffee.rb
@@ -1,5 +1,13 @@
 class Koffee < ApplicationRecord
   has_many :koffee_roasts
   has_many :roasts, through: :koffee_roasts
-  has_many :comments 
+  has_many :comments
+
+  def self.search(query)
+    coffees = Koffee.all
+    queryString = query.downcase
+    coffees.select do |coffee|
+      coffee.name.downcase.include?(queryString)
+    end
+  end 
 end
