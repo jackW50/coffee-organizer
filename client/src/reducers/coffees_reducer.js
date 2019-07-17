27203552c63795d3140738d7e31cf190e9c5094f
github username <github email address>
2019-07-17 13:25:37 -0500

    create basic coffees reducer that just returns state

---
+++ client/src/reducers/coffees_reducer.js
@@ -0,0 +1,25 @@
+function coffeesReducer (state = {
+  coffees: [],
+  coffeeSearch: []
+}, action) {
+
+  switch (action.type) {
+
+    case "FETCH_COFFEES";
+      return state
+
+    case "SEARCH_COFFEES":
+      return state
+
+    case "SEE_FAVORITES":
+      return state
+
+    default:
+      return state;
+
+  }
+}
+
+export default coffeesReducer;
+
+//coffees is an array of Coffee objects.
