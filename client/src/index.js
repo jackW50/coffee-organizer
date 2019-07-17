11ccea9e3bda8b3c87d3b03f4b99d0c1cf8d42fe
github username <github email address>
2019-07-17 13:14:12 -0500

    fix dependencies, create a store, and organize a home route within the react router framework

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowseRouter as Router, Route } from 'react-router-dom';
 import './index.css';
 import { createStore, applyMiddleware } from 'redux';
-import { Provider } from './reducers/rootReducer';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers/rootReducer';
 import thunk from 'redux-thunk';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+const Home = () => {
+  return(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  )
+}
+
+ReactDOM.render(
+  <Router>
+    <Route path="/" component={Home} />
+  </Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
