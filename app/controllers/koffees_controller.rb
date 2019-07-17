d7effcfb92dd5188fd502ed9004c0cb1418540f9
github username <github email address>
2019-07-17 12:28:41 -0500

    add destroy action to koffees controller

--- app/controllers/koffees_controller.rb
+++ app/controllers/koffees_controller.rb
@@ -10,4 +10,27 @@ class KoffeesController < ApplicationController
     end
     render json: @coffees
   end
+
+  def create
+    @coffee = Koffee.create(koffee_params)
+    render json: @coffee
+  end
+
+  def update
+    koffee = Koffee.find_by(params[:coffee][:id])
+    @coffee = Koffee.update(koffee_params)
+    render json: @coffee
+  end
+
+  def destroy
+    koffee = Koffee.find_by(params[:coffee][:id])
+    koffee.koffee_roasts.each do |koffee_roast|
+      koffee_roast.destory
+    end
+    koffee.comments.each do |comment|
+      comment.destroy
+    end
+    koffee.destroy
+  end 
+
 end
