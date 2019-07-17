41fb9ba61667e70807669d988dfa8080d722d6d8
github username <github email address>
2019-07-17 12:15:41 -0500

    add routes for koffees controller and comments controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :koffees, only: [:index, :create, :update, :destroy]
+  resources :comments, only: [:create, :destroy]
 end
