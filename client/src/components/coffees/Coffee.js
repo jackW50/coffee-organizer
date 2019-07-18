40b3c19e278a8c1369e2c7a8edc7ebb4be18b87e
github username <github email address>
2019-07-18 09:38:55 -0500

    add methods for component to handle attribute display for each coffee for favorite or certification

--- client/src/components/coffees/Coffee.js
+++ client/src/components/coffees/Coffee.js
@@ -4,6 +4,28 @@ import CommentsContainer from '../../containers/CommentsContainer.js'
 
 const Coffee = props => {
 
+  function certified() {
+    if (props.rainforestAllianceCertified) {
+      return "Rainforest Alliance Certified"
+    }
+  }
+
+  function favorite() {
+    if (!props.favorite) {
+      return (
+        <button onClick={handleFavoriteAdd}>Add to Favorites</button>
+      )
+    } else {
+      return (
+        <div>
+          <span className="favorite" >**Faved**</span>
+          <button onClick={handleRemoveFavorite}>Remove From Favorites</button>
+        </div>
+      )
+    }
+  }
+
+
   return (
     <div>
       <h5>{props.name}</h5>
