6f4eed257374304a77ebb24de2c35fd36a807353
github username <github email address>
2019-07-18 10:45:15 -0500

    create coffees container with connection to props and actions from the coffee actions file

---
+++ client/src/containers/CoffeesContainer.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Coffees from '../components/coffees/Coffees.js';
+import CoffeeAdd from '../components/coffees/CoffeeAdd.js';
+import CoffeeSearch from '../components/coffees/CoffeeSearch.js';
+import { seeFavorites, addCoffee, deleteCoffee, updateCoffee, searchCoffees } from '../actions/coffeeActions';
+
+
+class CoffeesContainer extends Component {
+
+  render() {
+    return(
+      <div>
+        <CoffeeSearch searchCoffees={this.props.searchCoffees} />
+        <Coffees coffees={this.props.coffees}
+          deleteCoffee={this.props.deleteCoffee}
+          updateCoffee={this.props.updateCoffee}
+        />
+
+
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    coffees: state.coffees.coffeeSearch
+  }
+}
+
+export default connect(mapStateToProps, { seeFavorites, addCoffee, deleteCoffee, updateCoffee })(CoffeesContainer);
