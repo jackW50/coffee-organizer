da44fa08136f47888c74ec915bd711bd16248c19
github username <github email address>
2019-07-18 10:22:12 -0500

    create CoffeeSearch Component

---
+++ client/src/components/coffees/CoffeeSearch.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+const CoffeeSearch = props => {
+
+  const [ name, setName ] = useState('')
+
+  function handleChange(e) {
+    setName(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const query = name
+    props.searchCoffees(query);
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={name} onChange={handleChange} />
+        <input type="submit" value="search coffee" />
+      </form>
+    </div>
+  )
+}
+
+export default (CoffeeSearch);
