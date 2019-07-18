f13d1223dac573eea0179923381e488002154c1e
github username <github email address>
2019-07-18 10:46:20 -0500

    update coffee actions file with included method calls from the Coffees container

--- client/src/actions/coffeeActions.js
+++ client/src/actions/coffeeActions.js
@@ -6,7 +6,23 @@ export function fetchCoffees() {
   }
 }
 
+export function seeFavorites() {
+  console.log('send api get request for only favorited coffees, and update state with response.')
+}
+
+export function searchCoffees() {
+  console.log('send api get req for coffees that include the query sent, and update state with the response.')
+}
+
+export function addCoffee() {
+  console.log('send create req to api and add newly created coffee to state.')
+}
+
 export function updateCoffee() {
   console.log('this will send and update req to server and update coffee');
   console.log('it will send back the updated coffee that we can change the state with');
+}
+
+export deleteCoffee() {
+  console.log('send delete req to api, and remove this coffee from state.')
 }
